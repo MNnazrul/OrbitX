d9954d03ddecaed8ad9cfa66a5fd48bd9496944a
MNnazrul <mnnazrulislam196130@gmail.com>
2025-01-24 21:48:27 +0600

    second col finished, bottom section develop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaFacebook, FaHackerrank, FaLinkedin } from "react-icons/fa";
+import { HiLocationMarker } from "react-icons/hi";
+import { MdCall, MdMessage } from "react-icons/md";
 
 function Footer() {
   return (
@@ -26,7 +29,7 @@ function Footer() {
           </div>
           {/* second col */}
           <div className="grid grid-cols-2 col-span-2 sm:grid-cols-3 md:pl-10">
-            <div className="">
+            <div>
               <div className="py-8 px-4">
                 <h1 className="text-xl font-bold mb-3 ">Quick Links </h1>
                 <ul className="flex flex-col gap-3">
@@ -45,6 +48,70 @@ function Footer() {
                 </ul>
               </div>
             </div>
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="text-xl font-bold mb-3 ">Quick Links </h1>
+                <ul className="flex flex-col gap-3">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Services</a>
+                  </li>
+                  <li>
+                    <a href="#">Login</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="text-xl font-bold mb-3 ">Contact US </h1>
+              </div>
+              <div className="space-y-3">
+                <div className="flex items-center gap-3">
+                  <HiLocationMarker />
+                  <p>Badarganj, Rangpur</p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <MdMessage />
+                  <p>mnnazrul@gmail.com</p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <MdCall />
+                  <p>+8801738430336</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* bottom section */}
+        <div className="hidden sm:block">
+          <div className="flex justify-between items-center ">
+            <span>copyright &copy; MNnazrul</span>
+            <div>
+              <a
+                href="https://web.facebook.com/nazrulislam.islam.7587/"
+                target="_blank"
+              >
+                <FaFacebook />
+              </a>
+              <a href="https://www.linkedin.com/in/mnnazrul/" target="_blank">
+                <FaLinkedin />{" "}
+              </a>
+              <a href="https://codeforces.com/profile/MNnazrul" target="_blank">
+                <FaHackerrank />
+              </a>
+            </div>
+            <span>
+              <ul>
+                <li>Privacy Policy</li>
+                <li>Term & Conditions</li>
+              </ul>
+            </span>
           </div>
         </div>
       </section>
