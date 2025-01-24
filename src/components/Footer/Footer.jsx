b20179332033772bef3641319b74d6c25df4dce8
MNnazrul <mnnazrulislam196130@gmail.com>
2025-01-24 16:44:54 +0600

    fist col done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
       <section className="max-w-[1200px] mx-auto text-white">
         <div className="grid md:grid-cols-3 py-3">
           {/* first col */}
-          <div>
+          <div className="py-8 px-4">
             <h1 className="text-xl sm:text-3xl font-bold sm:text-left text-justify mb-3">
               Be Ready To Grow
             </h1>
@@ -25,7 +25,27 @@ function Footer() {
             </div>
           </div>
           {/* second col */}
-          <div></div>
+          <div className="grid grid-cols-2 col-span-2 sm:grid-cols-3 md:pl-10">
+            <div className="">
+              <div className="py-8 px-4">
+                <h1 className="text-xl font-bold mb-3 ">Quick Links </h1>
+                <ul className="flex flex-col gap-3">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Services</a>
+                  </li>
+                  <li>
+                    <a href="#">Login</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
